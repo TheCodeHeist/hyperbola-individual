f8679680db4d808c2648e413157caa918352d459
Md. Riyasat Hossain <riyasat.hossain.29@gmail.com>
2024-11-25 18:42:18 +0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<main
+  class="w-full h-screen bg-slate-950 flex flex-col items-center justify-center gap-4"
+>
+  <img src="/logo.png" alt="logo" class="w-32 rounded-lg border shadow-lg shadow-sky-300" />
+
+  <p class="font-bold text-white text-5xl">
+    Welcome to <span class="text-teal-500">Hyperbola</span>
+  </p>
+
+  <p class="font-semibold text-white text-xl text-center">
+    Your ultimate free and open-source student management solution.
+  </p>
+</main>
